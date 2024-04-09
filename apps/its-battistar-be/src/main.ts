0cab945bd2d080f2231e14437b99e77293d4243b
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-09 14:22:11 +0200

    WIP eslint express and types

--- apps/its-battistar-be/src/main.ts
+++ apps/its-battistar-be/src/main.ts
@@ -14,8 +14,10 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to its-battistar-be!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT ?? 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
