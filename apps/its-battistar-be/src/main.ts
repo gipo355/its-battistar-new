acc5c3ee4517b39d47b9a009ce8d1001676baafd
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-09 14:36:26 +0200

    WIP ready

--- apps/its-battistar-be/src/main.ts
+++ apps/its-battistar-be/src/main.ts
@@ -3,18 +3,18 @@
  * This is only a minimal backend to get started.
  */
 
-import express from 'express';
+import express = require('express');
 import * as path from 'path';
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
+app.get('/api', (_, res) => {
   res.send({ message: 'Welcome to its-battistar-be!' });
 });
 
-const port = process.env.PORT ?? 3333;
+const port = process.env.PORT ?? '3333';
 
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
