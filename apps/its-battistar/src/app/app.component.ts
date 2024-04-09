09269726ac9f27db00ed3bcec2e1ad7b7f89887f
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-09 14:50:48 +0200

    WIP prepare bootstrap

--- apps/its-battistar/src/app/app.component.ts
+++ apps/its-battistar/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  NgbPaginationModule,
+  NgbAlertModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+
+    // ng-bootstrap modules
+    NgbPaginationModule,
+    NgbAlertModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
