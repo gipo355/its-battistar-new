c71276365c4eae06ee9c8c735a93185a2eab11b4
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-10 06:35:16 +0200

    WIP prepare mongo

--- apps/its-battistar-be/src/main.ts
+++ apps/its-battistar-be/src/main.ts
@@ -6,25 +6,35 @@ import mongoose from 'mongoose';
 import { buildApp } from './app';
 import { logger } from './utils/logger';
 
-const main = function () {
+const main = async function () {
   logger.info('🚀 Starting server...');
 
   const app = buildApp();
 
   const PORT = process.env.PORT ?? '3000';
+  const MONGO_STRING = process.env.MONGO_STRING;
+  if (!MONGO_STRING) {
+    logger.error('MONGO_STRING is not defined, database connection will fail.');
+    throw new Error(
+      'MONGO_STRING is not defined, database connection will fail.'
+    );
+  }
 
   logger.info(`⚡ Starting mongoose`);
   mongoose.set('debug', true);
-  mongoose
-    .connect('mongodb://127.0.0.1:27017/simulazione-01')
-    .then(() => {
-      app.listen(PORT, () => {
-        logger.info(`🚀 Server started on http://localhost:${PORT}`);
-      });
-    })
-    .catch((error: unknown) => {
-      logger.error('Error connecting to MongoDB', error);
-    });
+  await mongoose.connect(MONGO_STRING).catch((error: unknown) => {
+    logger.error('Error connecting to MongoDB', error);
+  });
+
+  app.on('close', () => {
+    logger.info('🚀 Server closed');
+  });
+  app.listen(PORT, () => {
+    logger.info(`🚀 Server started on http://localhost:${PORT}`);
+  });
 };
 
-main();
+main().catch(async (error: unknown) => {
+  logger.error(`Unexpected error: ${JSON.stringify(error)} `);
+  await mongoose.connection.close();
+});
