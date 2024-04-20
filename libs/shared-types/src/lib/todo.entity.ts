a7b4e5c34eaa8cceaa5b773309517f92ab595366
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-20 17:17:54 +0200

    WIP add todo entity

---
+++ libs/shared-types/src/lib/todo.entity.ts
@@ -0,0 +1,57 @@
+import { Type, Static } from '@sinclair/typebox';
+import fastJsonStringify from 'fast-json-stringify';
+import ajvInstance from '../utils/ajv';
+
+export const todoSchema = Type.Object({
+  id: Type.Optional(Type.String()),
+  title: Type.String(),
+  dueDate: Type.Date(),
+  completed: Type.Boolean(),
+  expired: Type.Boolean(),
+});
+
+export type TTodo = Static<typeof todoSchema>;
+
+export class Todo implements TTodo {
+  id?: string;
+
+  title: string;
+
+  dueDate: Date;
+
+  completed: boolean;
+
+  expired: boolean;
+
+  createdAt = new Date();
+
+  updatedAt = new Date();
+
+  constructor({
+    id,
+    title,
+    dueDate,
+    completed,
+    expired,
+  }: {
+    id?: string;
+    title: string;
+    dueDate: Date;
+    completed: boolean;
+    expired: boolean;
+  }) {
+    this.id = id;
+    this.title = title;
+    this.dueDate = dueDate;
+    this.completed = completed;
+    this.expired = expired;
+  }
+}
+
+/**
+ * @description must pass the data schema to the function to stringify the response
+ * Do it globally to cache the stringify function on startup
+ */
+export const stringifyTodo = fastJsonStringify(todoSchema);
+
+export const validateTodo = ajvInstance.compile(todoSchema);
