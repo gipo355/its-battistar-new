ec7d7bffeb115dbc94e39fb2e39583cd7d4e8d67
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-20 14:37:39 +0200

    WIP remove useless router

--- apps/its-battistar-be/src/errors/unsupported-method/unsupported-method.router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-import { unsupportedMethodHandler } from './unsupported-method.handler';
-
-const unsupportedMethodRouter = Router();
-unsupportedMethodRouter.route('*').put(unsupportedMethodHandler);
-export { unsupportedMethodRouter };
