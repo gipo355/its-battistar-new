0d657cc0d1aeb476a7b9583f3e621e47e0ea8f46
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-20 09:28:40 +0200

    feat(its-battistar-be): :sparkles: add catchAsync

--- apps/its-battistar-be/src/utils/catch-async.ts
+++ apps/its-battistar-be/src/utils/catch-async.ts
@@ -9,6 +9,7 @@ export const catchAsync = function catchAsync(
 ) {
   // we need to return the same function here to avoid calling it on assignment
   return function (request: Request, response: Response, next: NextFunction) {
+    // eslint-disable-next-line @typescript-eslint/use-unknown-in-catch-callback-variable
     routeHandlerFunction(request, response, next).catch(next);
   };
 };
