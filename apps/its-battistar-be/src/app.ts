7fe47ad146137e9bd539145a2b6f0b2ef2a4835f
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-20 15:00:26 +0200

    WIP remove broken import

--- apps/its-battistar-be/src/app.ts
+++ apps/its-battistar-be/src/app.ts
@@ -7,7 +7,6 @@ import { appRouter } from './api/app.router';
 import { NUMBER_OF_PROXIES } from './config';
 import { prepareMongo } from './db/mongo';
 import { environment } from './environment';
-import { errorsHandler } from './errors/errors.handler';
 import { errorsRouter } from './errors/errors.router';
 import { appMiddleware } from './middleware/app.middleware';
 import { logger } from './utils/logger';
