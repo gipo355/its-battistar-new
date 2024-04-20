78b004fc49943e7ff85ed237d9e284885bd02b89
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-20 10:48:30 +0200

    feat(its-battistar-be): :sparkles: add cookie parser

--- apps/its-battistar-be/src/app.middleware.ts
+++ apps/its-battistar-be/src/app.middleware.ts
@@ -5,15 +5,20 @@ import pino from 'pino-http';
 import { corsOptions, helmetOptions } from './config';
 import bodyParser = require('body-parser');
 import cors = require('cors');
+import cookieParser = require('cookie-parser');
+
 import { rateLimiterMiddleware } from './middleware/rate-limiter.middleware';
 
 const router = Router();
 
 router.use(cors(corsOptions));
+router.options('*', cors(corsOptions));
 
 router.use(helmet(helmetOptions));
 
 router.use(pino());
+
+router.use(cookieParser());
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
