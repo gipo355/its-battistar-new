e63336da0da1261271c890ea84107bba8c64a582
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-21 10:47:24 +0200

    WIP header component navigation

--- apps/its-battistar/src/app/shared/header/header.component.ts
+++ apps/its-battistar/src/app/shared/header/header.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class HeaderComponent {}
