6890b66058e6adec58df8f6641248676ad6370d8
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-21 13:34:51 +0200

    WIP add precommit prettier to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  '*.{css,scss,less,sass}': 'stylelint --fix --allow-empty-input',
+  '*.{css,scss,less,sass}':
+    'stylelint --fix --allow-empty-input && prettier --write',
   '*.{yml,md,js,ts,tsx,svelte,astro,cjs,mjs,cts,mts,jsx,json,json5,.jsonc}':
     'prettier --write',
   '*.{ts,tsx,js,jsx,svelte,astro,cjs,mjs,cts,mts,vue}':
-    'eslint --fix --ext .js,.ts,.tsx,.jsx,.svelte,.astro,.cjs,.mjs,.cts,.mts,.vue',
+    'eslint --fix --ext .js,.ts,.tsx,.jsx,.svelte,.astro,.cjs,.mjs,.cts,.mts,.vue && prettier --write',
   // '*.{html}': 'html-validate && htmlhint',
 };
