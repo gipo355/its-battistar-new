a265dea95cfc1bb78eeb8dd79b3727d445065bb4
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-21 10:34:46 +0200

    WIP prepare header component with BS

--- apps/its-battistar/src/app/app.component.ts
+++ apps/its-battistar/src/app/app.component.ts
@@ -2,11 +2,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { HeaderComponent } from './shared/header/header.component';
+
 @Component({
   standalone: true,
   imports: [
     RouterModule,
     HttpClientModule,
+
+    HeaderComponent,
 
     // ng-bootstrap modules
   ],
