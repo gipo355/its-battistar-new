6fa28fcd6a50237b6b0b000def4eecc5b82b9dc2
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-21 13:00:03 +0200

    WIP fix the spec file for header

--- apps/its-battistar/src/app/shared/header/header.component.spec.ts
+++ apps/its-battistar/src/app/shared/header/header.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -7,7 +9,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
+      imports: [HeaderComponent, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
