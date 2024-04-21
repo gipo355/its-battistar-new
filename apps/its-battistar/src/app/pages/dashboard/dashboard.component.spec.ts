24c9146f9ce8d4d725efde60af62757a76fa3d43
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-21 21:30:39 +0200

    feat(its-battistar): :sparkles: home component with tests fixed

--- apps/its-battistar/src/app/pages/dashboard/dashboard.component.spec.ts
+++ apps/its-battistar/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -7,7 +9,7 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent],
+      imports: [DashboardComponent, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
--- apps/its-battistar/src/app/pages/home/home.component.spec.ts
+++ apps/its-battistar/src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -7,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
