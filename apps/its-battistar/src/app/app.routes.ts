cdd66b3b9a48e4e5f1141dfa518dff6f19fc3540
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-21 19:43:30 +0200

    WIP add home page

--- apps/its-battistar/src/app/app.routes.ts
+++ apps/its-battistar/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  // { path: '', redirectTo: '/todos', pathMatch: 'full' },
+  // {
+  //   path: '',
+  //   redirectTo: '/todos',
+  //   pathMatch: 'full'
+  // },
+  {
+    path: '/',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+    pathMatch: 'full',
+  },
+
   // works with external component, but how would i inject stores and services?
   // {
   //   path: 'todos',
