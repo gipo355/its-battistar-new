9065667c89a58dbe263077eb771805a342736fba
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-22 11:46:28 +0200

    WIP add top bar component to dashboard

--- apps/its-battistar/src/app/pages/dashboard/dashboard.component.ts
+++ apps/its-battistar/src/app/pages/dashboard/dashboard.component.ts
@@ -3,13 +3,22 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
+import { MainContentComponent } from '../../components/main-content/main-content.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { TopBarComponent } from '../../components/top-bar/top-bar.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterModule, SidebarComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    SidebarComponent,
+    MainContentComponent,
+    RouterModule,
+    TopBarComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
