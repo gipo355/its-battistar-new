354f2c52ce3981368a97c3bd529c708e1b22f7d0
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-22 10:03:49 +0200

    feat(its-battistar): :sparkles: add sidebar component for dashboard

--- apps/its-battistar/src/app/pages/dashboard/dashboard.component.ts
+++ apps/its-battistar/src/app/pages/dashboard/dashboard.component.ts
@@ -3,12 +3,13 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterModule],
+  imports: [CommonModule, HeaderComponent, RouterModule, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
