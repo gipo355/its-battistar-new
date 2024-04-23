293f5458520413b76d0e0103fe0e636fce1e4e1c
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-23 11:17:39 +0200

    feat(its-battistar-be): :sparkles: prepare google callback handler for notes

--- apps/its-battistar-be/src/routes/auth/auth.router.ts
+++ apps/its-battistar-be/src/routes/auth/auth.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { googleCallbackHandler } from './google/google.handler';
 import { loginRouter } from './login/login.router';
 import { logoutRouter } from './logout/logout.router';
 import { refreshRouter } from './refresh/refresh.router';
@@ -14,5 +15,18 @@ r.use('/signup', signupRouter);
 r.use('/refresh', refreshRouter);
 
 r.use('/logout', logoutRouter);
+
+/**
+ * @openapi
+ * /auth/google/callback:
+ *  post:
+ *   tags:
+ *    - auth
+ *   description: google callback
+ *   responses:
+ *    200:
+ *     description: ok
+ */
+r.get('/google/callback', googleCallbackHandler);
 
 export { r as authRouter };
