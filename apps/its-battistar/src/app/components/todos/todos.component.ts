f11f1d2a8ccd95e435dbaad512fe863121a39bb9
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-24 19:11:22 +0200

    feat(its-battistar): :sparkles: init todos components html

--- apps/its-battistar/src/app/components/todos/todos.component.ts
+++ apps/its-battistar/src/app/components/todos/todos.component.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
+import { TodoFilterComponent } from './todo-filter/todo-filter.component';
+
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodoFilterComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
