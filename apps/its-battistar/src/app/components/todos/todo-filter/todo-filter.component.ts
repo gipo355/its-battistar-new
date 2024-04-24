53ff0c6a66fb02605bac6fe6061caac459015b26
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-24 11:07:30 +0200

    WIP add todo-filter component

---
+++ apps/its-battistar/src/app/components/todos/todo-filter/todo-filter.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-filter.component.html',
+  styleUrl: './todo-filter.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TodoFilterComponent {}
+
+function test() {}
+
+const x = 3;
