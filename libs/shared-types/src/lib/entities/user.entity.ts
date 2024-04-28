660d2927220e47aba7d7bb28e456624d4a393440
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-28 10:20:08 +0200

    WIP remove shared lib comment for ci test

--- libs/shared-types/src/lib/entities/user.entity.ts
+++ libs/shared-types/src/lib/entities/user.entity.ts
@@ -4,8 +4,6 @@ import fastJsonStringify from 'fast-json-stringify';
 import ajvInstance from '../../utils/ajv';
 import { accountsSchema, TAccount } from './account.entity';
 
-export const hello = 'hello';
-
 export const userSchemaUserInput = Type.Object({
   name: Type.String(),
   email: Type.String({
