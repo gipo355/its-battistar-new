1c08f4f727fa717398960defc4fd5b2813b1aa8b
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-28 15:10:07 +0200

    WIP remove useless environment

--- apps/its-battistar-angular/src/environments/environment.production.ts
+++
@@ -1,7 +0,0 @@
-import { IEnvironment } from './environment.entity';
-
-export const environment: IEnvironment = {
-  sentryDsn: '',
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
-};
