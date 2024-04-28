a6509e2cef5b8998027552f0589fc579f0b4fb07
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-04-28 18:20:43 +0200

    feat(error-page): :sparkles: init error page

---
+++ libs/error-page/src/lib/lib.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+export const errorPageRoutes: Route[] = [
+  { path: '', component: ErrorPageComponent },
+];
