49ea10123cb3dbc9a8a7969831ebbc052e6db503
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-02 10:23:59 +0200

    WIP change default github final redirect

--- apps/its-battistar-express/src/environments/environment.dev.ts
+++ apps/its-battistar-express/src/environments/environment.dev.ts
@@ -55,7 +55,7 @@ const optionalKeys: TOptionalEnvironment = {
   GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL ?? '',
   GITHUB_SCOPE: process.env.GITHUB_SCOPE ?? 'user:email',
   GITHUB_STATE: process.env.GITHUB_STATE ?? '3(#0/!~',
-  GITHUB_FINAL_REDIRECT: process.env.GITHUB_FINAL_REDIRECT ?? '/',
+  GITHUB_FINAL_REDIRECT: process.env.GITHUB_FINAL_REDIRECT ?? '',
 
   ENABLE_LOKI: process.env.ENABLE_LOKI ?? 'false',
 } as const;
