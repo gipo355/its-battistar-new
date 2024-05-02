d7ba1d8b9d6fb2cec0ce5829925be7999e9d16dd
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-02 10:23:28 +0200

    WIP add ajv errors and utility fn

--- libs/shared-utils/src/lib/utils/assertValidation.ts
+++ libs/shared-utils/src/lib/utils/assertValidation.ts
@@ -1,11 +1,20 @@
+import type { ValidateFunction } from 'ajv';
+import type { Logger } from 'pino';
+
 export function assertAjvValidationOrThrow<T>(
   data: unknown,
-  validatorFN: (data: unknown) => boolean,
-  error: Error
+  validatorFN: ValidateFunction,
+  error: Error,
+  logger?: Logger
 ): asserts data is T {
-  if (!validatorFN(data)) {
-    {
-      throw error;
-    }
+  const isValid = validatorFN(data);
+  if (!isValid) {
+    logger?.error({
+      message: error.message,
+      data,
+      errors: validatorFN.errors,
+    });
+
+    throw error;
   }
 }
