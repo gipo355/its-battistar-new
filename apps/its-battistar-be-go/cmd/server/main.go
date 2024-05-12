7cd1300db1bd58fda5396d9d7b776e8732241d6b
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-12 17:52:18 +0200

    WIP go taskfile and init command

--- apps/its-battistar-be-go/cmd/server/main.go
+++ apps/its-battistar-be-go/cmd/server/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "log/slog"
+
 func main() {
 	// Start the server
-	println("Hello, World!")
+	// println("Hello, World!")
+
+	for i := 0; i < 10; i++ {
+		slog.Info("Hello, World!")
+	}
 }
