b13e5d4e549c46f1ad0cd94791d5ea9f6f5e4121
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-13 22:14:16 +0200

    WIP prepare all nest routes for mongoloid api

--- libs/nest-auth/src/lib/nest-auth.controller.ts
+++ libs/nest-auth/src/lib/nest-auth.controller.ts
@@ -3,16 +3,16 @@ import { NestAuthService } from './nest-auth.service';
 import { RegisterNestAuthDto } from './dto/register-nest-auth.dto';
 import { LoginNestAuthDto } from './dto/login-nest-auth.dto';
 
-@Controller('nest-auth')
+@Controller()
 export class NestAuthController {
   constructor(private readonly nestAuthService: NestAuthService) {}
 
-  @Post()
+  @Post('register')
   register(@Body() createNestAuthDto: RegisterNestAuthDto) {
     return this.nestAuthService.register(createNestAuthDto);
   }
 
-  @Post()
+  @Post('login')
   login(@Body() loginNestAuthDto: LoginNestAuthDto) {
     return this.nestAuthService.login(loginNestAuthDto);
   }
--- libs/nest-users/src/lib/nest-users.module.ts
+++ libs/nest-users/src/lib/nest-users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [UsersModule],
-  imports: [UsersModule],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class NestUsersModule {}
