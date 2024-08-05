63a9034d1a7a1682570a5d4df7e40b67b1fa861a
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-08-05 12:25:52 +0200

    WIP prepare express

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import 'reflect-metadata';
 
-import express from 'express';
-import * as path from 'path';
+import mongoose from 'mongoose';
 
-const app = express();
+import app from './app';
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// eslint-disable-next-line no-magic-numbers
+const PORT = process.env.PORT ?? 3000;
 
-app.get('/api', (req, res) => {
-    res.send({ message: 'Welcome to backend!' });
-});
-
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+mongoose.set('debug', true);
+mongoose
+    .connect('mongodb://127.0.0.1:27017/simulazione-01')
+    .then(() => {
+        console.log('Connected to db');
+        app.listen(PORT, () => {
+            console.log('Server listening on port 3000');
+        });
+    })
+    .catch((err: unknown) => {
+        console.error(err);
+    });
---
+++ apps/backend/src/types/express.ts
@@ -0,0 +1,14 @@
+import type { NextFunction, Request, Response } from 'express';
+
+export type ErrorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => void;
+
+export type ExpressHandler = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => void;
